3fe8cb7bd230cecabea3217c968e42a3eaba5ab2
Rubens Mariuzzo <rubens@mariuzzo.com>
2013-01-31 18:23:40 -0400

    Detect placeholder `...` for single line code and underline it.

--- main.js
+++ main.js
@@ -5,6 +5,22 @@
  */
 $(function () {
 
-    
+    // Lookup for code in comments.
+
+    $('.UFICommentBody').each(function () {
+        var comment = $(this),
+            text = comment.text();
+        // Check for `code`...
+        if ((text.match(/`/g) || []).length > 1) {
+            comment.find(':not(:has(*), :empty)').each(function () {
+                var n = $(this),
+                    t = n.text(),
+                    c = t.match(/`(.+)`/);
+                if (c) {
+                    n.html(t.replace(/`(.+)`/, '<u>' + c[1] + '</u>'));
+                }
+            });
+        }
+    });
 
 });
